50cf85ac8f6f2a13cbe3fef1c7c3cc98ab852a1f
NinfarZ <tassolima2@gmail.com>
2023-03-27 01:34:12 +0100

    polish lighbox and product img

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -36,10 +36,14 @@ export default function ProductShowcase() {
 
 
                     <button onClick={handlePrevious} className={styles.btnPrevious}>
-                        <img width={10} src='/images/icon-previous.svg' />
+                        <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11 1 3 9l8 8" stroke="#1D2026" stroke-width="3" fill="currentColor" fill-rule="evenodd" />
+                        </svg>
                     </button>
                     <button onClick={handleNext} className={styles.btnNext}>
-                        <img width={10} src='/images/icon-next.svg' />
+                        <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+                            <path d="m2 1 8 8-8 8" stroke="#1D2026" stroke-width="3" fill="currentColor" fill-rule="evenodd" />
+                        </svg>
                     </button>
 
                 </figure>
@@ -94,16 +98,23 @@ function Lightbox({ image, toggleLightbox, previousButton, nextButton, handleThu
 
     return (
         <div className={styles.lightbox}>
-            <button onClick={toggleLightbox} className={styles.close}>
-                <img src='/images/icon-close.svg' />
-            </button>
+
             <div className={styles.imgWrapper}>
+                <button onClick={toggleLightbox} className={styles.close}>
+                    <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
+                        <path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#69707D" fill-rule="evenodd" />
+                    </svg>
+                </button>
                 <img className={styles.img} src={image} alt='product-img' />
                 <button onClick={previousButton} className={styles.btnPrevious}>
-                    <img width={10} src='/images/icon-previous.svg' />
+                    <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11 1 3 9l8 8" stroke="#1D2026" stroke-width="3" fill="currentColor" fill-rule="evenodd" />
+                    </svg>
                 </button>
                 <button onClick={nextButton} className={styles.btnNext}>
-                    <img width={10} src='/images/icon-next.svg' />
+                    <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+                        <path d="m2 1 8 8-8 8" stroke="#1D2026" stroke-width="3" fill="currentColor" fill-rule="evenodd" />
+                    </svg>
                 </button>
             </div>
             <div className={styles.thumbnails}>
